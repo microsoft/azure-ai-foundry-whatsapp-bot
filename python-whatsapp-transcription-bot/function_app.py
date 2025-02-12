b85216a9e5afe0e44a48fff58eb2801523f33e43
Eitan Sela <eitan.sela@gmail.com>
2025-02-12 19:42:04 +0200

    Update function_app.py
    Fixed hard coded `api_version` to get `AZURE_OPENAI_API_VERSION` environment variable.

--- python-whatsapp-transcription-bot/function_app.py
+++ python-whatsapp-transcription-bot/function_app.py
@@ -280,7 +280,7 @@ def transcribe_file(audio_file):
     
     client = AzureOpenAI(
         api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-        api_version="2024-02-01",
+        api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
         azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
     )
 
